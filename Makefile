5cdca1ec61af25c1aba33e679361370268f2ce79
Elias Meireles <eliasmeirelesf@gmail.com>
2025-03-22 11:03:21 -0300

    Update dependencies, refactor Makefile, and add path resolution
    Updated dependencies in `go.mod` and `go.sum` to the latest versions, improving library compatibility and security. Refactored the Makefile to simplify targets and remove unnecessary commands. Added a new utility `UserHomePathFix` for resolving paths with `~` to the user's home directory in `app_env.go`.

--- Makefile
+++ Makefile
@@ -1,14 +1,8 @@
-impl_test:
+test:
 	@go test ./...
 
-server-test:
-	@make codegen
-	@cd test && go test
+build:
+	@go build -o bin/$$(uname -m)/mock-server cmd/server/main.go
 
-codegen:
-	 @rm -rf test/gen/api.gen.go
-	 @oapi-codegen --config ./test/resource/config.yaml ./test/resource/pet-store.yaml
-
-pet-store:
-	 @oapi-codegen --config ./test/resource/local-config.yaml ./test/resource/pet-store.yaml  2>&1 | pbcopy
-
+update:
+	@go mod tidy
--- pkg/env/app_env.go
+++ pkg/env/app_env.go
@@ -22,6 +22,25 @@ func SetAppEnv(appEnv *AppEnv) {
 	env = appEnv
 }
 
+// UserHomePathFix resolves a path that starts with '~' to the user's home directory.
+// If the home directory cannot be determined, it logs an error and exits the application.
+// Parameters:
+//   - path: The file path string, potentially starting with '~'.
+//
+// Returns:
+//   - The resolved file path with '~' replaced by the user's home directory, or the original path if no substitution is needed.
+func UserHomePathFix(path string) string {
+	if strings.HasPrefix(path, "~") {
+		homeDir, err := os.UserHomeDir()
+		if err != nil {
+			fmt.Printf("Error: Unable to resolve user home directory for path: %v\n", err)
+			os.Exit(1)
+		}
+		path = strings.Replace(path, "~", homeDir, 1)
+	}
+	return path
+}
+
 func GetAppEnv() *AppEnv {
 	if env == nil {
 		serverConfig := flag.String("config", "", "The configuration file to use for the mock server")
@@ -50,6 +69,12 @@ func GetAppEnv() *AppEnv {
 			fmt.Println("Error: The 'mock' flag is required and cannot be empty.")
 			os.Exit(1)
 		}
+
+		*mockPath = UserHomePathFix(*mockPath)
+		*serverConfig = UserHomePathFix(*serverConfig)
+
+		fmt.Printf("Using server configuration file at: %s\n", *serverConfig)
+		fmt.Printf("Using mock data path at: %s\n", *mockPath)
 
 		env = &AppEnv{
 			Port:         *portFlag,
